9053bd99b2536dd08ed1b1c800e045fd9eccd018
Student <uauser@lab04.uantwerpen.be>
2018-07-30 16:56:38 +0200

    Fixed Timer Bean error

--- Chat/src/java/Facades/TimerSessionBean.java
+++ Chat/src/java/Facades/TimerSessionBean.java
@@ -8,41 +8,48 @@ package Facades;
 import EntityClasses.ChatUser;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.ejb.Schedule;
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 
 /**
  *
  * @author zhongxilu
  */
-@Stateless
+@Singleton
+@Startup
 public class TimerSessionBean {
 
 	@EJB
 	private ChatUserFacade chatUserFacade;
+        
+        @PostConstruct
+        public void init() {
+            final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+            executorService.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    checkOnlineUsers();
+                }
+            }, 0, 30, TimeUnit.SECONDS);    // schedule every 30 seconds
+        }
 
-	@PersistenceContext(unitName = "ChatPU")
-	private EntityManager em;
-
-	@Schedule(dayOfWeek = "*", month = "*", hour = "*", dayOfMonth = "*", year = "*", minute = "*", second = "*/10", persistent = false)
-	public void myTimer() {
-		TypedQuery<ChatUser> q = em.createNamedQuery("ChatUser.onlineUsers", ChatUser.class);
-		List<ChatUser> onlineUsers = q.getResultList();
-		Date currentTime = new Date();
-		for (ChatUser user : onlineUsers) {
-			long difference = Math.abs(currentTime.getTime() - user.getLastOnline().getTime()) / 1000;		// in seconds
-			if(difference > 30) {
-				// no request from this user in the past 30 seconds -> force log out
-				chatUserFacade.setIsOnline(user.getName(), false);
-			}
-
-		}
-	}
-
+        public void checkOnlineUsers() {
+            List<ChatUser> onlineUsers = chatUserFacade.getAllOnlineUsers();
+            Date currentTime = new Date();
+            for (ChatUser user : onlineUsers) {
+                long difference = Math.abs(currentTime.getTime() - user.getLastOnline().getTime()) / 1000;		// in seconds
+                if (difference > 30) {
+                    // no request from this user in the past 30 seconds -> force log out
+                    chatUserFacade.setIsOnline(user.getName(), false);
+                }
+            }
+        }
+        
 	// Add business logic below. (Right-click in editor and choose
 	// "Insert Code > Add Business Method")
 }
